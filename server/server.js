5abd19f44d4480ff3fe2465c113f5bf1489f3e98
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-06-29 09:04:08 -0300

    set up eslint+prettier

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-
 
 app.listen(port, () => console.log(`Server running on port ${port}!`));
